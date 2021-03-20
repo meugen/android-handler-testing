2dc32ad657e88b398c621fc6adf730796351c2e6
Yevhen Malyshev <meugen.82@gmail.com>
2021-03-21 00:51:49 +0200

    Fixed scripts to decrypt keystores

--- scripts/decrypt_properties_keystore.sh
+++ scripts/decrypt_properties_keystore.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gpg --quiet --batch --yes --decrypt --passphrase="KEYSTORE_PROPERTIES_PASSPHRASE" --output keystore/signing.properties keystore/signing.properties.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_PROPERTIES_PASSPHRASE" --output keystore/signing.properties keystore/signing.properties.gpg
