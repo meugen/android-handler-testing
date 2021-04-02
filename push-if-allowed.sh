9a55bdc299f17fdbc4b9b1ed6c2201fd5acf2f4b
Yevhen Malyshev <meugen.82@gmail.com>
2021-04-02 22:21:50 +0300

    Fixed scripts

--- push-if-allowed.sh
+++ push-if-allowed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./gradlew clean || exit 1
-./scripts/run_tests.sh
-./scripts/assemble_alpha_and_release.sh
+./scripts/run_tests.sh || exit 1
+./scripts/assemble_alpha_and_release.sh || exit 1
 git push
 
