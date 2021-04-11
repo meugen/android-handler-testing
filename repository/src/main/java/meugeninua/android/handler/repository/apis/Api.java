16fab88c2dcb14a6f9624cd306abe2f6009612bb
Yevhen Malyshev <meugen.82@gmail.com>
2021-04-11 21:33:33 +0300

    Added javadoc comment for API

--- repository/src/main/java/meugeninua/android/handler/repository/apis/Api.java
+++ repository/src/main/java/meugeninua/android/handler/repository/apis/Api.java
@@ -5,6 +5,10 @@ import retrofit2.http.GET;
 
 public interface Api {
 
+    /**
+     * Have a look at https://github.com/meugen/aws-lambdas/blob/main/src/main/java/meugeninua/awslambdas/SlowRestHandler.java for implementation details
+     * @return Call response
+     */
     @GET("/slow-rest")
     Call<String> slowRest();
 }
